5549866150184a9b14f1eaf718ae4f96fc97bc6d
Галавтдинов Станислав Сергеевич ИВТ-21-1Б <91310045+gals59911@users.noreply.github.com>
2023-04-08 16:45:47 +0300

    Add files via upload

---
+++ Binary_search/Binary_search/Binary_search/Binary_search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int mas[], int elem) {
+	int low = 0;
+	int high = sizeof(mas)-1;
+	while (low <= high) {
+		int mid = (high + low) / 2;
+		int ch = mas[mid];
+		if (ch == elem) {
+			return mid;
+		}
+		if (ch > elem) {
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1;
+		}
+	}
+	return -1;
+}
+int main()
+{ 
+	int a[] = {1,2,3,5,6,10};
+	cout<<binary_search(a, 3)<<endl;
+	cout << binary_search(a, 99) << endl;
+}
+
+
+
+
